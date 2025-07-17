0be504fdd798323c6f3242ae27c0dda180ce3be8
Paoneking <paoneking@gmail.com>
2025-07-18 00:05:58 +0545

    Build d5c5d29590bd3b0abef5a279c65124f52ebfb789

---
+++ VeexProvider/src/main/kotlin/com/paoneking/DataModel.kt
@@ -0,0 +1,47 @@
+package com.paoneking
+
+
+data class MovieItem(
+    val id: Int,
+    val type: String,
+    val title: String,
+    val label: String?,
+    val sublabel: String?,
+    val description: String,
+    val year: Int,
+    val imdb: Double,
+    val comment: Boolean,
+    val rating: Int,
+    val duration: String,
+    val downloadas: String,
+    val playas: String,
+    val classification: String?,
+    val image: String,
+    val cover: String,
+    val genres: List<Genre>,
+    val trailer: Trailer?,
+    val sources: List<Source>
+)
+
+data class Genre(
+    val id: Int,
+    val title: String
+)
+
+data class Trailer(
+    val id: Int,
+    val type: String,
+    val url: String
+)
+
+data class Source(
+    val id: Int,
+    val title: String,
+    val quality: String,
+    val size: String?,       // nullable, since it can be null
+    val kind: String,
+    val premium: String,
+    val external: Boolean,
+    val type: String,
+    val url: String
+)
---
+++ VeexProvider/src/main/kotlin/com/paoneking/VeexPlugin.kt
@@ -0,0 +1,15 @@
+package com.paoneking
+
+import android.content.Context
+import com.lagradost.cloudstream3.CommonActivity.activity
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class VeexPlugin: Plugin() {
+
+    override fun load(context: Context) {
+        // All providers should be added in this manner
+        registerMainAPI(VeexProvider())
+    }
+}
