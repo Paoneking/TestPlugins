bf689a476a355655d76c5797301d8bdf19d2d27f
Paoneking <paoneking@gmail.com>
2025-07-18 00:43:09 +0545

    .

--- VeexProvider/src/main/kotlin/com/paoneking/DataModel.kt
+++ VeexProvider/src/main/kotlin/com/paoneking/DataModel.kt
@@ -7,20 +7,20 @@ data class MovieItem(
     val title: String,
     val label: String?,
     val sublabel: String?,
-    val description: String,
-    val year: Int,
-    val imdb: Double,
-    val comment: Boolean,
-    val rating: Int,
-    val duration: String,
-    val downloadas: String,
-    val playas: String,
+    val description: String?,
+    val year: Int?,
+    val imdb: Double?,
+    val comment: Boolean?,
+    val rating: Int?,
+    val duration: String?,
+    val downloadas: String?,
+    val playas: String?,
     val classification: String?,
-    val image: String,
-    val cover: String,
-    val genres: List<Genre>,
+    val image: String?,
+    val cover: String?,
+    val genres: List<Genre>?,
     val trailer: Trailer?,
-    val sources: List<Source>
+    val sources: List<Source>?
 )
 
 data class Genre(
@@ -38,10 +38,11 @@ data class Source(
     val id: Int,
     val title: String,
     val quality: String,
-    val size: String?,       // nullable, since it can be null
+    val size: String?,
     val kind: String,
-    val premium: String,
+    val premium: String?,
     val external: Boolean,
     val type: String,
     val url: String
 )
+
